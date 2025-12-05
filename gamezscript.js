55ebe7ea9f08bc5c6103a4484c123125ce0746d6
Emil Østberg <emilostberg23@gmail.com>
2025-12-05 23:56:37 +0100

    :3

---
+++ gamezscript.js
@@ -0,0 +1,13 @@
+
+
+document.getElementById("shufflebutton").addEventListener("click", shuffleWallpaper);
+let wallpaperindex = 0;
+function shuffleWallpaper() {
+    wallpaperindex++;
+    const wallpapersarray = ["url(localresources/pixelartwallpaper1.png)","url(localresources/pixelartwallpaper2.png)","url(localresources/pixelartwallpaper3.jpg)","url(localresources/pixelartwallpaper4.jpg)"]
+    if (wallpaperindex >= wallpapersarray.length) {
+        wallpaperindex = 0;
+    }
+    document.body.style.backgroundImage = wallpapersarray[wallpaperindex];
+
+}
