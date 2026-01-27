d1fa0935b669d42fd2871fb781f79ec5045e9aba
Emil Østberg <emilostberg23@gmail.com>
2026-01-27 09:17:17 +0100

    Added DOOM

--- rockscript.js
+++ rockscript.js
@@ -5,6 +5,35 @@ const chatbutton = document.getElementById("chatbutton");
 let saveddata = [];
 
 
+    // Set the date we're counting down to
+    let countDownDate = new Date("Dec 18, 2026 12:00:00").getTime();
+
+    // Update the count down every 1 second
+    let x = setInterval(function() {
+
+    // Get today's date and time
+    let now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    let distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("demo").innerHTML = days + "d<br> " + hours + "h<br> "
+    + minutes + "m<br> " + seconds + "s ";
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+}
+}, 1000);
+
 document.addEventListener('play', function(e){
     var audios = document.getElementsByTagName('audio');
     for(var i = 0, len = audios.length; i < len;i++){
